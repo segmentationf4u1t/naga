ccd82ca1f22db62c33310ca43ac08ef0f8db1515
SegFault <cryptexunion@gmail.com>
2023-11-23 21:25:36 +0100

    dc update

--- conf/cfg.ts
+++ conf/cfg.ts
@@ -8,7 +8,7 @@ const Core = {
     // Service Description
     description: 'Explore the frontier of artificial intelligence with NagaAi. Offering stable and reliable API access to the latest in AI technology, NagaAi empowers developers and businesses to harness the power of cutting-edge and bleeding-edge AI solutions. Elevate your projects with next-gen AI capabilities.',
     // Service Discord
-    discord: 'https://discord.gg/8AQcyNF8EB',
+    discord: 'https://discord.naga.ac/',
 
 }
 export default Core
